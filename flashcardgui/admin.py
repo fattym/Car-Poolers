0d51e07b09533c5b48d02899c1d3fe3c5237d0a3
fattym <jnganga288@gmail.com>
2020-06-10 12:22:19 +0300

    take

--- flashcardgui/admin.py
+++ flashcardgui/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from api.models import Deck
+
+
+class DeckAdmin(admin.ModelAdmin):
+        pass
+
+admin.site.register(Deck, DeckAdmin)
